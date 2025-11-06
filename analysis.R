1d532dd098526a78834ee84623acebd7aa63bc4a
Siling Guo <silingguo95@gmail.com>
2025-11-06 15:36:23 -0800

    filter

--- analysis.R
+++ analysis.R
@@ -5,15 +5,16 @@ library(tidyverse)
 real_estate <- read.csv("Real_Estate_Sales_2001-2023_GL.csv")
 
 # take columns we want
-real_estate_simp <- real_estate %>% select("List.Year", "Town", "Assessed.Value", "Sale.Amount", "Sales.Ratio", "Property.Type", "Residential.Type")
+real_estate_simp <- real_estate %>% select("List.Year", "Town", "Assessed.Value", "Sale.Amount", "Sales.Ratio", "Property.Type", "Residential.Type")%>%
+  filter(!is.na(List.Year) & List.Year != "" & !is.na(Town) & Town != "" & !is.na(Assessed.Value) & Assessed.Value != "" & !is.na(Sale.Amount) & Sale.Amount != "" & !is.na(Sales.Ratio) & Sales.Ratio != "" & !is.na(Property.Type) & Property.Type != "" &!is.na(Residential.Type) & Residential.Type!="" )
 
 # clean data
 
 
 # descriptive statistics over entire data set
-mean_sp <- mean(real_estate$Sale.Amount)
-mean_av <- mean(real_estate$Assessed.Value)
-mean_sr <- mean(real_estate$Sales.Ratio)
+mean_sp <- mean(real_estate_simp$Sale.Amount)
+mean_av <- mean(real_estate_simp$Assessed.Value)
+mean_sr <- mean(real_estate_simp$Sales.Ratio)
 
 # min
 
