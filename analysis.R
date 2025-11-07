c591109c0e2fc25b16489d848b7f24089c1a094b
Megan Joseph <meganajoseph@berkeley.edu>
2025-11-07 08:59:59 -0800

    trying to pull

--- analysis.R
+++ analysis.R
@@ -8,7 +8,8 @@ real_estate <- read.csv("Real_Estate_Sales_2001-2023_GL.csv")
 real_estate_simp <- real_estate %>% select("List.Year", "Town", "Assessed.Value", "Sale.Amount", "Sales.Ratio", "Property.Type", "Residential.Type")
 
 # clean data
-
+# drop duplicates
+real_estate_dropped <- real_estate_simp[duplicated(real_estate_simp), ]
 
 # descriptive statistics over entire data set
 mean_sp <- mean(real_estate$Sale.Amount)
